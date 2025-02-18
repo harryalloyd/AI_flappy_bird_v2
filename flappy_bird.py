496262aaaa9dea2cecd22dbbb0bdbbab571e05af
harryalloyd <harrisonalloyd@gmail.com>
2025-02-18 12:10:35 -0800

    fixed bug where the background and pipes weren't displaying properly

--- flappy_bird.py
+++ flappy_bird.py
@@ -155,7 +155,7 @@ class Base:
         
 
 def draw_window(win, bird, pipes, base):
-        win.blit(BASE_IMG, (0,0))
+        win.blit(BACKGROUND_IMG, (0,0))
         for pipe in pipes:
             pipe.draw(win)
             
@@ -172,6 +172,8 @@ def main():
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
     
+    score = 0
+    
     
     run = True
     while run:
@@ -181,11 +183,28 @@ def main():
                 run = False
                 
         #bird.move()
+        add_pipe = False
+        remove = []
         for pipe in pipes:
             if pipe.collide(bird):
                 pass
-            
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                remove.append(pipe)
+                
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+                
+                
             pipe.move()
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(700))
+        
+        for r in remove:
+            pipes.remove(r)
+            
+            
         base.move()
         draw_window(win, bird, pipes, base)
         
