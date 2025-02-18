d51a3f914b64b2e63d46184f13a86207b8108cb5
harryalloyd <harrisonalloyd@gmail.com>
2025-02-18 15:01:11 -0800

    Score functionality

--- flappy_bird.py
+++ flappy_bird.py
@@ -3,6 +3,7 @@ import neat
 import time
 import os
 import random
+pygame.font.init()
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
@@ -16,6 +17,10 @@ BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bi
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BACKGROUND_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
+
+
 
 class Bird:
     IMGS = BIRD_IMGS
@@ -154,10 +159,13 @@ class Base:
         
         
 
-def draw_window(win, bird, pipes, base):
+def draw_window(win, bird, pipes, base, score):
         win.blit(BACKGROUND_IMG, (0,0))
         for pipe in pipes:
             pipe.draw(win)
+            
+        text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
+        win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
             
         base.draw(win)
         
@@ -204,9 +212,12 @@ def main():
         for r in remove:
             pipes.remove(r)
             
+        if bird.y + bird.img.get_height() >= 730:
+            pass
+            
             
         base.move()
-        draw_window(win, bird, pipes, base)
+        draw_window(win, bird, pipes, base, score)
         
     pygame.quit()
     quit()
